a9db52978f0536f1d571e7050c236035f5ddf7de
William Booker <wbooker14@gmail.com>
2021-01-28 12:47:37 -0500

    Update cnvpytor_slurm.sh

--- cnvpytor_slurm.sh
+++ cnvpytor_slurm.sh
@@ -10,18 +10,18 @@
 module load anaconda
 source activate dros_sv
 
-cnvpytor -root test1_A1.pytor -rd -chrom 2R 2L 3R 3L 4 X Y A1.merge.sorted.bam
-cnvpytor -root test1_A2.pytor -rd -chrom 2R 2L 3R 3L 4 X Y  A2.merge.sorted.bam
-cnvpytor -root test1_A3.pytor -rd -chrom 2R 2L 3R 3L 4 X Y  A3.merge.sorted.bam
-cnvpytor -root test1_A4.pytor -rd -chrom 2R 2L 3R 3L 4 X Y  A4.merge.sorted.bam
-cnvpytor -root test1_A5.pytor -rd -chrom 2R 2L 3R 3L 4 X Y  A5.merge.sorted.bam
-cnvpytor -root test1_A6.pytor -rd -chrom 2R 2L 3R 3L 4 X Y  A6.merge.sorted.bam
-cnvpytor -root test1_A7.pytor -rd -chrom 2R 2L 3R 3L 4 X Y  A7.merge.sorted.bam
-cnvpytor -root test1_AB8.pytor -rd -chrom 2R 2L 3R 3L 4 X Y  AB8.merge.sorted.bam
-cnvpytor -root test1_B1.pytor -rd -chrom 2R 2L 3R 3L 4 X Y  B1.merge.sorted.bam
-cnvpytor -root test1_B2.pytor -rd -chrom 2R 2L 3R 3L 4 X Y  B2.merge.sorted.bam
-cnvpytor -root test1_B3.pytor -rd -chrom 2R 2L 3R 3L 4 X Y  B3.merge.sorted.bam
-cnvpytor -root test1_B4.pytor -rd -chrom 2R 2L 3R 3L 4 X Y  B4.merge.sorted.bam
-cnvpytor -root test1_B5.pytor -rd -chrom 2R 2L 3R 3L 4 X Y  B5.merge.sorted.bam
-cnvpytor -root test1_B6.pytor -rd -chrom 2R 2L 3R 3L 4 X Y  B6.merge.sorted.bam
-cnvpytor -root test1_B7.pytor -rd -chrom 2R 2L 3R 3L 4 X Y  B7.merge.sorted.bam
+cnvpytor -root test1_A1.pytor -chrom 2R 2L 3R 3L 4 X Y -rd A1.merge.sorted.bam
+cnvpytor -root test1_A2.pytor -chrom 2R 2L 3R 3L 4 X Y -rd A2.merge.sorted.bam
+cnvpytor -root test1_A3.pytor -chrom 2R 2L 3R 3L 4 X Y -rd A3.merge.sorted.bam
+cnvpytor -root test1_A4.pytor -chrom 2R 2L 3R 3L 4 X Y -rd A4.merge.sorted.bam
+cnvpytor -root test1_A5.pytor -chrom 2R 2L 3R 3L 4 X Y -rd A5.merge.sorted.bam
+cnvpytor -root test1_A6.pytor -chrom 2R 2L 3R 3L 4 X Y -rd A6.merge.sorted.bam
+cnvpytor -root test1_A7.pytor -chrom 2R 2L 3R 3L 4 X Y -rd A7.merge.sorted.bam
+cnvpytor -root test1_AB8.pytor -chrom 2R 2L 3R 3L 4 X Y -rd AB8.merge.sorted.bam
+cnvpytor -root test1_B1.pytor -chrom 2R 2L 3R 3L 4 X Y -rd B1.merge.sorted.bam
+cnvpytor -root test1_B2.pytor -chrom 2R 2L 3R 3L 4 X Y -rd B2.merge.sorted.bam
+cnvpytor -root test1_B3.pytor -chrom 2R 2L 3R 3L 4 X Y -rd B3.merge.sorted.bam
+cnvpytor -root test1_B4.pytor -chrom 2R 2L 3R 3L 4 X Y -rd B4.merge.sorted.bam
+cnvpytor -root test1_B5.pytor -chrom 2R 2L 3R 3L 4 X Y -rd B5.merge.sorted.bam
+cnvpytor -root test1_B6.pytor -chrom 2R 2L 3R 3L 4 X Y -rd B6.merge.sorted.bam
+cnvpytor -root test1_B7.pytor -chrom 2R 2L 3R 3L 4 X Y -rd B7.merge.sorted.bam
